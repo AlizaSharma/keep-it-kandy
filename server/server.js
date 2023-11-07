7619478b8cde7c01e32632c1cb7e1d9f7c14f2b3
Mark Anthony <touya1313@hotmail.com>
2023-11-06 19:15:42 -0700

    keep getting errors in server -_-

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -38,5 +39,3 @@ const startApolloServer = async () => {
 };
 
 startApolloServer();
-
-// test
