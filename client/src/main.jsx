09a353a9c914246db087ef47adf3d361e754eee9
Gama0194 <134178399+Gama0194@users.noreply.github.com>
2023-11-06 20:53:37 -0600

    added pages and components folders

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
+import 'semantic-ui-css/semantic.min.css'
 import './index.css'
+import Homepage from './pages/homepage.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Homepage />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />
+      }, 
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
