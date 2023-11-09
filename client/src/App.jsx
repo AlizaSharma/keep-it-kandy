2652a3b56c06dd4dff8a6bc6f4f7f7d93d573836
Mark Anthony <touya1313@hotmail.com>
2023-11-09 02:27:55 -0700

    added token code to app.jsx in client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,39 @@ import 'semantic-ui-css/semantic.min.css'
 import './App.css'
 import Navbar from './components/navbar.jsx'
 
+// GraphQL and Tokens lines 9-38. Tied to auth.js in utils client. Haven't been able to test this.
+import {
+  ApolloClient,
+  InMemoryCache, // Not sure if an ApolloProvider should go in here or not. 
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
 function App() {
   const [count, setCount] = useState(0)
 
