c124e8540dfc887716dfc0a1c34fb11aa2e6d690
Gama0194 <134178399+Gama0194@users.noreply.github.com>
2023-11-08 19:55:30 -0600

    added pages folder and connected react router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,19 +5,38 @@ import App from './App.jsx'
 import 'semantic-ui-css/semantic.min.css'
 import './index.css'
 import Homepage from './pages/homepage.jsx'
+import Store from './pages/store.jsx'
+import Signin from './pages/sign-in.jsx'
+import Signup from './pages/sign-up.jsx'
+import Cart from './pages/cart.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <Homepage />,
-    children: [
-      {
-        index: true,
-        element: <Homepage />
-      }, 
-    ],
-  },
+    errorElement: <div>Not found</div>,       
+  },{
+    path: '/',
+    element: <Homepage />,
+    errorElement: <div>Not found</div>,       
+  },{
+    path: '/store',
+    element: <Store />,
+    errorElement: <div>Not found</div>,
+  },{
+    path: '/signin',
+    element: <Signin />,
+    errorElement: <div>Not found</div>,
+  },{
+    path: '/signup',
+    element: <Signup />,
+    errorElement: <div>Not found</div>,
+  },{
+    path: '/cart',
+    element: <Cart />,
+    errorElement: <div>Not found</div>,
+  }
+
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
