81f8d36386384671e43c486b76bd6fcb73e56bbe
Aliza Sharma <aliza.sharma12@gmail.com>
2023-11-09 17:08:05 -0600

    front end layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Navbar from './components/navbar.jsx'
+import Body from './components/Body'
 import 'bulma/css/bulma.min.css';
+
 
 function App() {
   return (
     <>    
       <div>
+      < Navbar />
         < Header/>
-        < Navbar />
+        <main>
+        < Body />
+        </main>
         < Footer/>
     
       </div>
---
+++ client/src/components/Body.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import 'bulma/css/bulma.min.css';
+import '../App.css';
+
+const Body = () => {
+    return (
+        <section class="section">
+        <div class="container">
+          <div id="slider">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-16by9">
+                  <img src="./public/assets/image3.jpg" alt="" />
+                  <div class="overlay">
+                    <a href="#" class="button is-info is-light is-large">Shop Now</a>
+                  </div>
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Body;
