c655a66f32e4b5abb92b76131d210b3333703cf0
Mark Anthony <touya1313@hotmail.com>
2023-11-09 03:10:02 -0700

    fixed error in auth.js server side syntax

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -10,29 +10,29 @@ module.exports = {
       code: 'UNAUTHENTICATED',
     },
   }),
-  // authMiddleware({ req }) {
-  //   let token = req.body.token || req.query.token || req.headers.authorization;
+  authMiddleware({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-  //   if (req.headers.authorization) {
-  //     token = token.split(' ').pop().trim();
-  //   }
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
 
-  //   if (!token) {
-  //     return req;
-  //   }
+    if (!token) {
+      return req;
+    }
 
-  //   try {
-  //     const { data } = jwt.verify(token, secret, { maxAge: expiration });
-  //     req.user = data;
-  //   } catch (err) {
-  //     console.error(err);
-  //     console.log('Invalid token');
-  //   }
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch (err) {
+      console.error(err);
+      console.log('Invalid token');
+    }
 
-  //   return req;
-  // },
-  signToken({ firstName, email, _id }) {
-    const payload = { firstName, email, _id };
+    return req;
+  },
+  signToken({ username, _id }) {
+    const payload = { username, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
