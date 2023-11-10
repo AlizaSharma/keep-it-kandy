6c64b44a2f0309f91351cf7862bcfaa38d201af3
Aliza Sharma <aliza.sharma12@gmail.com>
2023-11-09 21:40:47 -0600

    updated shop page

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -1,3 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar.jsx';
+import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
+
+function Shop() {
+    return (
+        <>
+        < Navbar />
+        < Header />
 <section class="section">
 <div class="container">
     <div class="columns is-multiline">
@@ -5,7 +15,7 @@
             <div class="card product-card">
                 <div class="card-image">
                     <figure class="image is-4by3">
-                        <img src="product1.jpg" alt="Product 1">
+                        <img src="product1.jpg" alt="Product 1"/>
                     </figure>
                 </div>
                 <div class="card-content">
@@ -21,7 +31,7 @@
             <div class="card product-card">
                 <div class="card-image">
                     <figure class="image is-4by3">
-                        <img src="product2.jpg" alt="Product 2">
+                        <img src="product2.jpg" alt="Product 2"/>
                     </figure>
                 </div>
                 <div class="card-content">
@@ -37,7 +47,7 @@
             <div class="card product-card">
                 <div class="card-image">
                     <figure class="image is-4by3">
-                        <img src="product3.jpg" alt="Product 3">
+                        <img src="product3.jpg" alt="Product 3"/>
                     </figure>
                 </div>
                 <div class="card-content">
@@ -59,7 +69,7 @@
             <div class="card product-card">
                 <div class="card-image">
                     <figure class="image is-4by3">
-                        <img src="product1.jpg" alt="Product 1">
+                        <img src="product1.jpg" alt="Product 1"/>
                     </figure>
                 </div>
                 <div class="card-content">
@@ -75,7 +85,7 @@
             <div class="card product-card">
                 <div class="card-image">
                     <figure class="image is-4by3">
-                        <img src="product2.jpg" alt="Product 2">
+                        <img src="product2.jpg" alt="Product 2"/>
                     </figure>
                 </div>
                 <div class="card-content">
@@ -91,7 +101,7 @@
             <div class="card product-card">
                 <div class="card-image">
                     <figure class="image is-4by3">
-                        <img src="product3.jpg" alt="Product 3">
+                        <img src="product3.jpg" alt="Product 3"/>
                     </figure>
                 </div>
                 <div class="card-content">
@@ -105,3 +115,9 @@
     </div>
 </div>
 </section>
+       
+< Footer />
+        </>
+    );
+}
+export default Shop;
