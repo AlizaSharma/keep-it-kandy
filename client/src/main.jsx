96469970f1645cb6d5d18df25576bd8e5734c7f2
Gama0194 <134178399+Gama0194@users.noreply.github.com>
2023-11-09 19:28:55 -0600

    corrected react router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import Candy from './pages/Candy.jsx'
+import Chocolate from './pages/Chocolate.jsx'
+import Contact from './pages/Contact.jsx'
+import Explore from './pages/Explore.jsx'
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <div>Not found</div>,
+  },
+  {
+    path: '/candy',
+    element: <Candy />,
+    errorElement: <div>Not found</div>,
+  },
+  {
+    path: '/chocolate',
+    element: <Chocolate />,
+    errorElement: <div>Not found</div>,
+  },
+  {
+    path: '/contact',
+    element: <Contact />,
+    errorElement: <div>Not found</div>,
+  },
+  {
+    path: '/explore',
+    element: <Explore />,
+    errorElement: <div>Not found</div>,
+  },
+]);
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -44,6 +77,6 @@ ReactDOM.render(
 //   }
 
 // ]);
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <RouterProvider router={router} />
-// );
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
