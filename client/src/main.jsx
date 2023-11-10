199cd070f2b47461de9ff8fd1b7470c3798866b0
Gama0194 <134178399+Gama0194@users.noreply.github.com>
2023-11-09 20:00:56 -0600

    cleaned up main.jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -35,48 +35,9 @@ const router = createBrowserRouter([
     errorElement: <div>Not found</div>,
   },
 ]);
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
 
-// import Homepage from './pages/Candy.jsx'
-// import Store from './pages/Contact.jsx'
-// import Signin from './pages/Login.jsx'
-// import Signup from './pages/Chocolate.jsx'
-// import Cart from './pages/Explore.jsx'
 
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <App />,
-//     errorElement: <div>Not found</div>,       
-//   },{
-//     path: '/',
-//     element: <Homepage />,
-//     errorElement: <div>Not found</div>,       
-//   },{
-//     path: '/store',
-//     element: <Store />,
-//     errorElement: <div>Not found</div>,
-//   },{
-//     path: '/signin',
-//     element: <Signin />,
-//     errorElement: <div>Not found</div>,
-//   },{
-//     path: '/signup',
-//     element: <Signup />,
-//     errorElement: <div>Not found</div>,
-//   },{
-//     path: '/cart',
-//     element: <Cart />,
-//     errorElement: <div>Not found</div>,
-//   }
-
-// ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 );
