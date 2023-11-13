0591b387503f4fc6e878e08d4502067e6ea19f64
OliveraDiaz <130876122+OliveraDiaz@users.noreply.github.com>
2023-11-12 20:23:03 -0600

    added DB name to connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/UPDATE-NAME-HERE'); // check name here later
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/keep-it-kandy'); // check name here later
 
 module.exports = mongoose.connection;
